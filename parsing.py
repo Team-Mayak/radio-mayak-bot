b7ba8925ad94a7403c24ea9f358003b2e5d1bc36
fo4dog <ub25@mail.ru>
2019-04-02 19:30:10 +0300

    upgrade

--- parsing.py
+++ parsing.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import re
 from  urllib.request import*
+print(ord("А"))
 url = "https://radiomayak.ru/podcasts/archive/"
 def get_html(url):
     req = Request(url)
@@ -12,7 +13,6 @@ list = soup.find_all(class_="b-podcast__block-link")
 slovar_sound =[]
 arhiv_podkastov = []
 for a in list:
-
     print(a['title'])
     arhiv_podkastov.append(a['href'])
 
@@ -22,9 +22,30 @@ vibor_podkasta = int(input('напишите номер подкаста, кот
 second_html = get_html('https://radiomayak.ru'+ arhiv_podkastov[vibor_podkasta])
 second_soup = BeautifulSoup(second_html, "html.parser")
 sound = second_soup.find_all(class_= "b-podcast__records-listen")
-
 for b in sound:
-    print(b['data-title'])
     slovar_sound.append(b['data-url'])
+sound = second_soup.find_all(class_= "b-podcast__records-url")
+name_audio = ''
+namesau = []
+sc = 0
+for c in sound:
+    if len(c['title']) != 0:
+        name_audio = c['title']
+    else:
+        c = str(c)
+        for i in range(1, len(c)):
+            if c[i] == '>':
+                sc= 1
+
+            if (1072 <= ord(c[i]) <= 1104 or 1040 <= ord(c[i]) <= 1072) and sc == 0:
+                name_audio += c[i]
+            if (1072 <= ord(c[i-1]) <= 1104 or 1040 <= ord(c[i]) <= 1072) and sc == 0 and (c[i] == '.' or c[i] == '"'):
+                name_audio += ' '
+            if (1072 <= ord(c[i-1]) <= 1104 or 1040 <= ord(c[i]) <= 1072) and sc == 0 and c[i] =='=':
+                name_audio += ' '
+    namesau.append(name_audio)
+    name_audio = ''
+    sc = 0
+
 vibor_audio = int(input('напишите номер аудиозаписи, которыю хотите открыть'))
-print(slovar_sound[vibor_audio])
+print(namesau[vibor_audio], slovar_sound[vibor_audio])
