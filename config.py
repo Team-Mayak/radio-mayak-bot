8ea9a476ee2b01a1ad945e6700a1cd185e9729b9
olegok111 <olegok2003@mail.ru>
2019-04-05 21:09:48 +0300

    Added tips

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-AUTH = False
-TOKEN = ''
-PROXY = 'https://208.88.233.1:54149'
-USERNAME = ''
-PASSWORD = ''
+AUTH = False # change to True if proxy needs a username and a password to use
+TOKEN = '' # paste the token here
+PROXY = 'https://208.88.233.1:54149' # this proxy address may be changed
+USERNAME = '' # username if authentication is needed
+PASSWORD = '' # password if required
 if not AUTH:
     REQUEST_KWARGS = {'proxy_url': PROXY}
 else:
